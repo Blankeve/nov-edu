56e3619e500c4772719b7830ebdf26df13f64229
dzyx <dzyx@edu.com>
2021-12-07 11:04:31 +0800

    后端初始化

---
+++ server/nov-edu/src/test/java/com/edu/nov/NovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.nov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
