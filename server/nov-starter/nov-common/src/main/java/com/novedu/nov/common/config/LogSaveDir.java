e99a6afff082efe32f72032b6aa12c296e71b60f
dzyx <dzyx@edu.com>
2021-12-08 17:10:16 +0800

    PropertyDefinerBase动态选择日志目录

---
+++ server/nov-starter/nov-common/src/main/java/com/novedu/nov/common/config/LogSaveDir.java
@@ -0,0 +1,33 @@
+package com.novedu.nov.common.config;
+
+import ch.qos.logback.core.PropertyDefinerBase;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author ：juam
+ * @date ：2021/12/8 16:41
+ * @description：
+ * @modified By：
+ * @version:
+ */
+@Component
+public class LogSaveDir extends PropertyDefinerBase  {
+
+    @Override
+    public String getPropertyValue() {
+        System.out.println("PropertyDefiner Start...");
+        String logDir="";
+        String os = System.getProperty("os.name").toLowerCase();
+        System.out.println("os:"+os);
+        if(os.indexOf("win")!=-1){
+            String user = System.getenv("USERNAME");
+            logDir = String.format("C:/Users/%s/Documents/nov_log",user);
+        }
+        else if(os.indexOf("lin")!=-1){
+            logDir = "/usr/local/java/nov_log";
+        }
+        System.out.println("logDir:"+logDir);
+        return logDir;
+    }
+
+}
