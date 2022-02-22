29b7907d89c0de10478935a51f4fd357b371289b
dzyx <dzyx@edu.com>
2022-02-22 17:27:22 +0800

    权限管理后端继续

---
+++ server/nov-starter/nov-service/service_ucenter/src/main/java/com/novedu/nov/ucenter/entity/vo/AclRolePermissionVO.java
@@ -0,0 +1,78 @@
+package com.novedu.nov.ucenter.entity.vo;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * <p>
+ * 权限
+ * </p>
+ *
+ * @author juam
+ * @since 2022-02-22
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ApiModel(value="AclPermission对象", description="权限")
+public class AclRolePermissionVO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @JsonSerialize(using = ToStringSerializer.class)
+    @ApiModelProperty(value = "编号")
+    @TableId(value = "id", type = IdType.NONE)
+    private Long id;
+
+    @ApiModelProperty(value = "所属上级")
+    private Long parentId;
+
+    @NotBlank(message = "权限名称不能为空")
+    @ApiModelProperty(value = "权限名称")
+    private String name;
+
+    @ApiModelProperty(value = "类型(1:菜单,2:按钮)")
+    private Integer type;
+
+    @NotBlank(message = "权限值不能为空")
+    @ApiModelProperty(value = "权限值")
+    private String value;
+
+    @ApiModelProperty(value = "访问路径")
+    private String path;
+
+    @ApiModelProperty(value = "组件路径")
+    private String component;
+
+    @ApiModelProperty(value = "图标")
+    private String icon;
+
+    @ApiModelProperty(value = "状态(0:禁止,1:正常)")
+    private Integer status;
+
+    @ApiModelProperty(value = "逻辑删除 1（true）已删除， 0（false）未删除")
+    private Integer isDeleted;
+
+    @TableField(value = "create_time", fill = FieldFill.INSERT)
+    @ApiModelProperty(value = "创建时间")
+    private Date createTime;
+
+    @TableField(value = "update_time", fill = FieldFill.INSERT_UPDATE)
+    @ApiModelProperty(value = "更新时间")
+    private Date updateTime;
+
+    @TableField(exist = false)
+    private List<AclRolePermissionVO> children;
+}
