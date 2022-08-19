0d01874d670b8aa63a17b796d7b5aad74c8065f9
PS2022FCIZWONB\Admin <yltl@dev.com>
2022-08-19 17:08:50 +0800

    代码调整

--- client/vue-front-template/utils/request.js
+++ client/vue-front-template/utils/request.js
@@ -5,7 +5,7 @@ import { removeToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  baseURL: process.env.VUE_APP_BASE_API || 'http://159.75.234.20:8100', // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 10000 // request timeout
 })
